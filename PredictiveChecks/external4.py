3795dae48a33f452b5a74b850ae1875af8c92f82
malin-horstmann <malin.horstmann@icloud.com>
2023-03-28 10:38:56 +0200

    added plt.savefig()

--- PredictiveChecks/external4.py
+++ PredictiveChecks/external4.py
@@ -4,6 +4,7 @@ left to fix:
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
 import json
 import pytensor 
 import pymc as pm
@@ -198,3 +199,25 @@ def sampling(prepared_model, n_samples):
         prior_pred = pm.sample_prior_predictive(n_samples)
 
         return post_data, post_pred, prior_pred
+
+
+def plot_ppc(model, plot_name, obs, post_pred, prior_pred):
+    nBins = len(model.expected_actualdata(model.config.suggested_init()))
+    plt.step(np.linspace(0,nBins-1,nBins),prior_pred.prior_predictive.main[0].T, alpha = 0.01, c = 'steelblue', where = 'mid');
+    plt.vlines(np.arange(nBins),*np.quantile(prior_pred.prior_predictive.main[0],[.15,.85],axis=0), colors = 'steelblue', label='prior');
+
+        ## Posterior predictive
+    plt.step(np.linspace(0,nBins-1,nBins),post_pred.posterior_predictive.main[0].T, alpha = 0.01, c = 'orange', where = 'mid');
+    plt.vlines(np.arange(nBins),*np.quantile(post_pred.posterior_predictive.main[0],[.15,.85],axis=0), colors = 'orange', label='posterior')
+
+        ## Observations
+    plt.scatter(np.arange(nBins), obs, c = 'k',s=12, zorder = 999, label = "data")
+    plt.legend(loc='upper left')
+
+    plt.title(f'Predictive checks for {plot_name}')
+
+    plt.savefig(f'{plot_name}')
+    plt.show()
+
+    return 
+
