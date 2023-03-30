fd2af5d66977cca4e7c8ba4c37e8cdbf2b9cd9c4
malin-horstmann <malin.horstmann@icloud.com>
2023-03-30 11:59:30 +0200

    ..

--- src/pyhf_pymc/MH_inference.py
+++ src/pyhf_pymc/MH_inference.py
@@ -15,6 +15,12 @@ from pytensor.graph.basic import Apply
 from pytensor.graph import Apply, Op
 from contextlib import contextmanager
 
+import sys
+sys.path.insert(1, '/Users/malinhorstmann/Documents/pyhf_pymc/src')
+from pyhf_pymc import prepare_inference
+
+####
+
 class ExpDataClass(pt.Op):
     """
     Input: 
@@ -42,100 +48,7 @@ class ExpDataClass(pt.Op):
         outputs[0][0] = np.asarray(result, dtype=node.outputs[0].dtype)
 
 
-def prepare_priors(model, unconstr_dict):
-    """
-    Preparing priors for model preparation
-    Input: 
-        - pyhf model
-        - dictionary of all unconstrained parameters 
-    Output: 
-        - dictionary with all parameters (unconstrained, normal, poisson)
-    """
 
-    unconstr_mu, unconstr_sigma, norm_mu, norm_sigma, poiss_pars = [], [], [], [], []
-    norm_poiss_dict = {}
-    
-    ## Add normal priors to dictionary
-    for k,v in model.config.par_map.items():
-        if isinstance(v['paramset'], pyhf.parameters.constrained_by_normal):
-            a, b  = [], []
-            for i in model.constraint_model.viewer_aux.selected_viewer._partition_indices[model.config.auxdata_order.index(k)]:
-                a.append(model.config.auxdata[int(i)])
-                b.append(model.constraint_model.constraints_gaussian.sigmas[int(i)])
-            norm_poiss_dict[k] = {'type': 'normal', 'input': [a, b]}
-
-    ## Add poisson priors to dictionary
-    for k,v in model.config.par_map.items():
-        if isinstance(v['paramset'], pyhf.parameters.constrained_by_poisson):
-            a = []
-            for i in model.constraint_model.viewer_aux.selected_viewer._partition_indices[model.config.auxdata_order.index(k)]:
-                a.append(model.config.auxdata[int(i)]**3)
-            norm_poiss_dict[k] = {'type': 'poisson', 'input': [a, a]}
-
-    return {**unconstr_dict, **norm_poiss_dict}
-
-def get_target(model):
-    """
-    Ordering vector for the parameters
-    Input: 
-        - pyhf model
-    Output: 
-        - index vector
-    """
-
-    target = []
-    unconstr_idx, norm_idx, poiss_idx = [], [], []
-    norm_poiss_dict = {}
-
-    for k, v in model.config.par_map.items():
-
-        if isinstance(v['paramset'], pyhf.parameters.unconstrained):
-            unconstr_idx = np.concatenate([
-                np.arange(v['slice'].start,v['slice'].stop) for k,v in model.config.par_map.items() if isinstance(v['paramset'], pyhf.parameters.unconstrained)
-                ])
-            pass
-
-        if isinstance(v['paramset'], pyhf.parameters.paramsets.constrained_by_normal):
-            norm_idx = np.concatenate([
-                np.arange(v['slice'].start,v['slice'].stop) for k,v in model.config.par_map.items() if isinstance(v['paramset'], pyhf.parameters.constrained_by_normal)
-                ])
-            pass
-
-        if isinstance(v['paramset'], pyhf.parameters.constrained_by_poisson):
-            poiss_idx = np.concatenate([
-                np.arange(v['slice'].start,v['slice'].stop) for k,v in model.config.par_map.items() if isinstance(v['paramset'], pyhf.parameters.constrained_by_poisson)
-                ])
-            pass
-
-    for i in [unconstr_idx, norm_idx, poiss_idx]:
-        i = np.array(i)
-        if i.size != 0:
-            target.append(i)
-    target = np.concatenate(target)
-
-    return target
-
-
-
-def prepare_model(model, observations, priors, precision):
-    """
-    Preparing model for sampling
-    Input: 
-        - pyhf model
-        - observarions
-        - dictionary of priors
-        - model precision
-    Output: 
-        - dictinonary of the model with keys 'model', 'obs', 'priors', 'precision'
-    """
-
-    model_dict = {}
-    model_dict['model'] = model
-    model_dict['obs'] = observations
-    model_dict['priors'] = priors
-    model_dict['precision'] = precision
-
-    return model_dict
 
 
 def sampling(prepared_model, n_samples):
@@ -184,7 +97,7 @@ def sampling(prepared_model, n_samples):
             if i.size != 0:
                 pars.append(i)
         pars = np.concatenate(pars)
-        target = get_target(model)
+        target = prepare_inference.get_target(model)
         final = pt.as_tensor_variable(pars[target.argsort()].tolist())
         
         mainOp = ExpDataClass('mainOp', jax.jit(model.expected_actualdata))
