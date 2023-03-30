e6d1c843e33131709646d123cfe77d855ac337f7
malin-horstmann <malin.horstmann@icloud.com>
2023-03-30 11:28:51 +0200

    add HMC module

---
+++ src/pyhf_pymc/HMC_inference.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import json
+import pymc as pm
+
+import pyhf
+pyhf.set_backend('jax')
+
+from jax import grad, jit, vmap, value_and_grad, random
+import jax
+import jax.numpy as jnp
+import pytensor
+from pytensor import tensor as pt
+from pytensor.graph.basic import Apply
+from pytensor.graph import Apply, Op
+from contextlib import contextmanager
