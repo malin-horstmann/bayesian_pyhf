4b9d1cda74e8e43a31774630acd5894be6c24405
malin-horstmann <malin.horstmann@icloud.com>
2023-04-18 09:54:22 +0200

    just savin the nested class

--- src/pyhf_pymc/actual_inference.py
+++ src/pyhf_pymc/actual_inference.py
@@ -24,10 +24,24 @@ import arviz as az
 from pyhf_pymc import prepare_inference
 
 
-def make_ops(model):
-    """
+class sampling:
 
-    """
+    def __init__(self, model):
+        self.model = model
+        self.inner = self.ExpDataOp()
+    
+    @jax.jit
+    def processed_expData(self, parameters):
+        return model.expected_actualdata(parameters)
+
+    jitted_processed_expData = jax.jit(processed_expData)
+
+    # Gradient list (dn_bins/dx_1, ..., dn_bins/dx_nPars)
+    @jax.jit
+    def vjp_expData(self, pars, tang_vec):
+        _, back = jax.vjp(processed_expData, pars)
+        return back(tang_vec)[0]
+    
     class ExpDataOp(Op):
 
         itypes = [pt.dvector]  
@@ -35,50 +49,39 @@ def make_ops(model):
 
         def perform(self, node, inputs, outputs):
             (parameters, ) = inputs
-
-            jitted_processed_expData = jax.jit(model.expected_actualdata)
             results = jitted_processed_expData(parameters)
 
-            if len(outputs) == 1:
-                    outputs[0][0] = np.asarray(results)
-                    return
-            for i, r in enumerate(results):
-                    outputs[i][0] = np.asarray(r)
+            # if len(outputs) == 1:
+            #         outputs[0][0] = np.asarray(results)
+            #         return
+            # for i, r in enumerate(results):
+            #         outputs[i][0] = np.asarray(r)
+            outputs[0][0] = np.asarray(results)
 
         # def grad(self, inputs, output_gradients):
         #     (parameters,) = inputs
         #     (tangent_vector,) = output_gradients
         #     return [vjp_op(parameters, tangent_vector)]
-        
+
     expData_op = ExpDataOp()
 
-    return expData_op
-       
-def sampling(prepared_model, n_samples, n_chains, sampling_method):
-    """
+    def actual_sampling(self, prepared_model):
+        obs = prepared_model['obs']
+        prior_dict = prepared_model['priors']
+        precision = prepared_model['precision']
 
-    """
-    model = prepared_model['model']
+           
 
-    expData_op = make_ops(model)
+        with pm.Model() as m:
+            pars = prepare_inference.priors2pymc(prepared_model)
 
-    obs = prepared_model['obs']
-    # prior_dict = prepared_model['priors']
-    precision = prepared_model['precision']
-
-    with pm.Model():
-        pars = prepare_inference.priors2pymc(prepared_model)
-
-
-        Expected_Data = pm.Normal("Expected_Data", mu=expData_op(pars), sigma = precision, observed=obs)
-        
-        step1 = pm.Metropolis()
-
-        if sampling_method == 'Metropolis':
-            post_data = pm.sample(n_samples, chains=n_chains, cores=4, step=step1)
-
-        post_pred = pm.sample_posterior_predictive(post_data)
-        prior_pred = pm.sample_prior_predictive(n_samples)
+            # Expected_Data = pm.Poisson("Expected_Data", mu=expData_op(pars), observed=obs)
+            Expected_Data = pm.Normal("Expected_Data", mu=expData_op(pars), sigma = precision, observed=obs)
+            
+            step1 = pm.Metropolis()
+            
+            post_data = pm.sample(draws=1000, chains = 1, cores=4, step=step1, )
+            post_pred = pm.sample_posterior_predictive(post_data)
+            prior_pred = pm.sample_prior_predictive(100)
 
         return post_data, post_pred, prior_pred
-
